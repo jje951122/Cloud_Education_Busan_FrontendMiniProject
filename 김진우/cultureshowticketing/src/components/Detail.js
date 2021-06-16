84c7d7cc3c78141d25f50f86fced105412568390
Jinwoo, Kim <jinwookim@leaprealm.com>
2021-06-16 16:11:19 +0900

    20210616

--- 김진우/cultureshowticketing/src/components/Detail.js
+++ 김진우/cultureshowticketing/src/components/Detail.js
@@ -90,15 +90,14 @@ const Detail = ({
           <div className="detailContentTitle">
             <b>꼭 읽어주세요</b>
           </div>
-          <div>
+          <p className="infoText">
             게시판 운영 규정에 어긋난다고 판단되는 게시글은 사전 통보없이
             블라인드 처리될 수 있습니다.
-          </div>
-          <div>
-            특히 티켓 매매 및 양도의 글은 발견 즉시 임의 삭제되며 전화번호,
-            이메일 등의 개인정보는 악용될 우려가 있으므로 게시를 삼가 주시기
+          </p>
+          <p className="infoText">
+            특히 티켓 매매 및 양도의 글은 발견 즉시 임의 삭제되며 개인정보는 악용될 우려가 있으므로 게시를 삼가 주시기
             바랍니다.
-          </div>
+          </p>
           <UserComment></UserComment>
         </TabPane>
       </Tabs>
