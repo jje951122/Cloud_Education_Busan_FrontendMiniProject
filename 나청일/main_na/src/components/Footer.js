11e1eb7d5e3128438f2976c259179db6e819b95b
EhitEpit <chungil987@naver.com>
2021-06-17 14:31:44 +0900

    메인 병합

---
+++ 나청일/main_na/src/components/Footer.js
@@ -0,0 +1,10 @@
+// import React from "react";
+// import "./footer.css";
+
+// const Footer = () => (
+
+//   <div>
+//     <footer className="footer"> © My Company Name. </footer>
+//   </div>
+// );
+// export default Footer
---
+++ 나청일/main_na/src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => (
+
+  <header className="header">
+    <h1 className="hd">머리</h1>
+    </header>
+);
+export default Header;
